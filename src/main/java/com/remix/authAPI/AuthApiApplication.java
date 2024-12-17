6312b4e6540f86784b4fa903378aa2038b38f0dc
Tojo1512 <tsarahoela5@gmail.com>
2024-12-17 12:05:42 +0300

    Config main

--- src/main/java/com/remix/authAPI/AuthApiApplication.java
+++ src/main/java/com/remix/authAPI/AuthApiApplication.java
@@ -8,6 +8,7 @@ public class AuthApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApiApplication.class, args);
+		System.out.println("Server is running on port 8080");
 	}
 
 }
